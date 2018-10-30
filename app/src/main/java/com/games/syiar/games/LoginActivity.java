549a89c33d650ca503604aeff66b1186923ebad7
gembulman <cumaliataj@gmail.com>
2018-10-30 21:23:17 +0700

    first commit

---
+++ app/src/main/java/com/games/syiar/games/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.games.syiar.games;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
